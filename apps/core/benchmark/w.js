a0f5f4dbc5dc74383a842574b772dd2be7c8fae5
ace <504595380@qq.com>
2022-05-03 18:29:31 +0800

    type

--- apps/core/benchmark/w.js
+++ apps/core/benchmark/w.js
@@ -3,10 +3,10 @@ const { V2, V1 } = require("../dist");
 parentPort.once("message", ({ type, data }) => {
   let g;
   if (type === "v1") {
-    g = V1.deserialize(data);
+    g = V1.Graph.deserialize(data);
   }
   if (type === "v2") {
-    g = V2.deserialize(data);
+    g = V2.Graph.deserialize(data);
   }
   parentPort.postMessage(g.serialize());
 });
